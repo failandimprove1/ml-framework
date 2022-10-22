e91ae857880907ab64652252e45da3893678a02f
johannes <johannes.ronning@tietoevry.com>
2022-10-22 15:08:02 +0200

    making sure github actions fails on errorcode -1

--- tests/test.cpp
+++ tests/test.cpp
@@ -9,7 +9,7 @@ void test_create_file()
 {
 	fstream file = create_file("test_file.txt");
 	test.is_equal(!file.is_open(), "Expecting file to not be open after creating file");
-	test.is_equal(!!file, "Expecting file to exist");
+	test.is_equal(!file, "Expecting file to exist");
 };
 
 int main()
